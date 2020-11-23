65f3bf4e00e1a989f83f8d39855bfbfa343fe2bf
s_bertailak <s_bertailak@72-S_BERTAILAK>
2020-11-23 17:54:57 +0600

    test cases

--- alem-cup-2020/src/Test.java
+++ alem-cup-2020/src/Test.java
@@ -1,15 +1,6 @@
 
 import java.util.LinkedList;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author berta
- */
 public class Test {
 
     public static void main(String[] args) {
@@ -29,3 +20,43 @@ public class Test {
         queue.add(1);
     }
 }
+
+
+//osinda eger jol jok bolsa, bar jagina karay kashu kerek, 
+//bombosi bolmasa kasu kerek
+//13 11 1 100
+//.............
+//.!.!.!.!.!;!;
+//............;
+//;!.!.!.!.!;!;
+//;...........;
+//.!;!.!.!.!.!.
+//......;.....;
+//.!;!.!.!.!;!.
+//;.....;.....;
+//;!;!.!.!;!.!.
+//;..;..;......
+//4
+//m 0 8 3 0 0
+//m 0 8 4 0 0
+//p 1 10 0 0 0
+//b 1 10 0 5 2
+
+
+//monster korse orninga kalip jatr, kashu kerek //ili turu kerek?
+//13 11 1 184
+//.............
+//.!.!.!.!.!.!.
+//.............
+//;!.!.!.!.!.!.
+//;............
+//.!;!.!.!.!.!.
+//......;......
+//.!;!.!.!.!.!.
+//;.....;.....;
+//;!;!.!.!;!.!.
+//;..;..;......
+//3
+//p 1 10 6 0 0
+//b 1 10 6 5 2
+//m 0 10 7 0 0
